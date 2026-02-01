b15cc3fb924749128c3930704bed25c1507ed7f8
Koo Ho Yin <jackkoo1207@gmail.com>
2026-02-01 14:12:30 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jackkoo1207.github.io',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/portfolio-website/'
 })
