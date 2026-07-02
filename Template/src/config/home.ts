ab77379170d7980d6a4b828c36c6acaa51a0f168
jackkoo1207 <jackkoo1207@gmail.com>
2026-07-03 00:50:57 +0800

    Updates

--- Template/src/config/home.ts
+++
@@ -1,8 +0,0 @@
-export const homeConfig = {
-  greeting: "Hello, I'm Koo Ho yin,Jack.",
-  description: "A passionate developer with a keen eye for AI-integrated web applications, Agentic AI and robotics.",
-  buttons: {
-    viewProjects: "View Projects",
-    readPosts: "Read Posts"
-  }
-} as const 
